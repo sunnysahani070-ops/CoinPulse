973577813956f1957b68c2659fae3f9423a58950
sunny <sunnysahani070@gmail.com>
2026-06-10 00:26:09 +0530

    First commit

---
+++ lib/coingecko.actions.ts
@@ -0,0 +1,75 @@
+'use server';
+
+import qs from 'query-string';
+
+const BASE_URL = process.env.COINGECKO_BASE_URL as string;
+const API_KEY = process.env.COINGECKO_API_KEY as string;
+
+if (!BASE_URL) throw new Error('Could not get base url');
+if (!API_KEY) throw new Error('Could not get api key');
+
+export async function fetcher<T>(
+    endpoint: string,
+    params?: QueryParams,
+    revalidate = 60,
+): Promise<T> {
+    const url = qs.stringifyUrl(
+        {
+            url: `${BASE_URL}/${endpoint}`,
+            query: params,
+        },
+        { skipEmptyString: true, skipNull: true },
+    );
+
+    const apiKeyHeader = BASE_URL.includes('pro-api') ? 'x-cg-pro-api-key' : 'x-cg-demo-api-key';
+
+    const response = await fetch(url, {
+        headers: {
+            [apiKeyHeader]: API_KEY,
+            'Content-Type': 'application/json',
+        } as Record<string, string>,
+        next: { revalidate },
+    });
+
+    if (!response.ok) {
+        const errorBody: CoinGeckoErrorBody = await response.json().catch(() => ({}));
+
+        throw new Error(`API Error: ${response.status}: ${JSON.stringify(errorBody)} `);
+    }
+
+    return response.json();
+}
+
+export async function getPools(
+    id: string,
+    network?: string | null,
+    contractAddress?: string | null,
+): Promise<PoolData> {
+    const fallback: PoolData = {
+        id: '',
+        address: '',
+        name: '',
+        network: '',
+    };
+
+    if (network && contractAddress) {
+        try {
+            const poolData = await fetcher<{ data: PoolData[] }>(
+                `/onchain/networks/${network}/tokens/${contractAddress}/pools`,
+            );
+
+            return poolData.data?.[0] ?? fallback;
+        } catch (error) {
+            console.log(error);
+            return fallback;
+        }
+    }
+
+    try {
+        const poolData = await fetcher<{ data: PoolData[] }>('/onchain/search/pools', { query: id });
+
+        return poolData.data?.[0] ?? fallback;
+    } catch {
+        return fallback;
+    }
+}
